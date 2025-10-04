1de36b08965df4d243c8e92a7de365de15a730eb
joelhuba <joelhuba1999@gmail.com>
2025-10-04 09:40:46 -0500

    Actualiza entidades y repositorios para nueva estructura
    Se ha añadido la tabla "Users" en `UserEntity.cs` y se han actualizado los métodos de creación y actualización de usuarios en `IUsersBLL` y `UsersRepository.cs` para utilizar nuevos DTOs. Se ha definido la interfaz `ITasksRepository` con métodos para gestionar tareas. Además, se han añadido referencias a paquetes necesarios en los archivos de proyecto y se han realizado limpiezas en varios archivos de configuración y código.

--- ToDoList.Core/Interfaces/BLL/IUsersBLL.cs
+++ ToDoList.Core/Interfaces/BLL/IUsersBLL.cs
@@ -7,8 +7,8 @@ namespace ToDoList.Core.Interfaces.BLL
     {
         Task<ResponseDTO> GetUserById(int idUser);
         Task<ResponseDTO> GetAllUsers(PaginatorDTO paginatorDTO, string? email = null);
-        Task<ResponseDTO> CreateUser(UserDTO user);
-        Task<ResponseDTO> UpdateUser(UserDTO user);
+        Task<ResponseDTO> CreateUser(CreateUserDTO user);
+        Task<ResponseDTO> UpdateUser(UpdateUserDTO user);
         Task<ResponseDTO> DeleteUser(int idUser);
     }
 }
